8a898d6504a37d5d6d33313b51ad9c8561e76b50
Sasha Poulain <sashapoulain5@gmail.com>
2023-03-29 11:48:53 +0300

    Number ve number metodları, Math ve random

--- udemy.js
+++ udemy.js
@@ -11,6 +11,110 @@ let f = function() { alert('hi') } //FONKSİYON
 let fS = String(f) //FUNCTION TO S
 let arr = [12, 13, 14] //ARRAY 
 let arrS = String(arr) //ARR TO S
-document.body.innerHTML = ` ${arrS} ${typeof arrS}`;
-console.log(value);
-console.log(typeof value);
+
+
+
+
+
+
+let pi = '3.14'; //string bir number
+let piTn = Number(pi) //stringi number a çevirdik
+
+let nll = null; //null
+let piTnll = Number(nll); //null to number ve değeri 0 oldu
+
+let t = 'hello world' //string
+let ttT = Number(t); //değeri nan türü number oldu
+
+let arr2 = [1, 2, 4, 3]; //object yani  array
+let arrToN = Number([arr2]) //değeri nan türü number oldu
+
+
+
+let m = Math.PI; //değeri 3.14 türü number
+let e = Math.E; //değeri 2.71 türü number
+
+
+//round .5 ve üzerini yukarı yuvarlar .5 den daha azını aşağı yuvarlar
+let u = Math.round(3.9) //değeri 4 number türünde
+let u2 = Math.round(3.5) //değeri 4 number türünde
+let u3 = Math.round(3.3);
+
+//ceil hep yuvarı yuvarlar
+let c = Math.ceil(2.6) //3
+let c2 = Math.ceil(2.5) //3
+let c3 = Math.ceil(2.4) //3
+
+
+
+
+//floor aşağı yuvarlar
+let f1 = Math.floor(2.6) //2
+let f2 = Math.floor(2.5) //2
+let f3 = Math.floor(2.4) //2
+
+
+
+
+
+//sqrt karekök alır.
+
+let s = Math.sqrt(16) //4
+let s2 = Math.sqrt(64) //8
+
+
+//abs mutlak değer alır
+
+let a = Math.abs(-10); //10
+
+//pow : sayının yanındaki sayı kuvvetini alır. 2 üzeri 4 nedir gibi
+let p = Math.pow(2, 4) //16
+let p2 = Math.pow(4, 3) //64
+
+
+
+//max ve min değer
+
+
+let maxV = Math.max(-1, 100, 3) //100
+let minV = Math.min(-1, 100, 3) //-1
+
+
+
+
+
+
+//random sayı 
+let r = Math.floor(Math.random() * 10); //0 ve 10 arasında 0 dahil
+let rBir = (Math.floor(Math.random() * 10) + 1); //0 ve 10 arasında 0 haric
+
+
+
+
+
+
+
+
+
+
+document.body.innerHTML = `Value:  ${rBir} `;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(value);
+// console.log(typeof value);
